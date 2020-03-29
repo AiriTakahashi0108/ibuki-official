e5f0e25c7d19bc06edc63c180a36c35d1b4a97d1
高橋愛理 <a.takahashi@ragate.co.jp>
2020-03-29 22:55:16 +0900

    大体完了

---
+++ pages/discography/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="discographyContainer">
+    <aside class="sideMenu">
+
+    </aside>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "discography"
+  }
+</script>
+
+<style scoped>
+
+</style>
--- pages/discography/request.vue
+++ pages/discography/request.vue
@@ -1,26 +1,13 @@
 <template>
-  <div class="profileContainer">
-    <div class="profile content">
-      <pageTitle :title="pageTitle" :detail="pageDetail"/>
-    </div>
-  </div>
+  $END$
 </template>
 
-
-<style>
-  .news {
-    color: var(--basic-fontColor-dark);
-  }
-</style>
 <script>
-  import PageTitle from "@/components/parts/PageTitle";
   export default {
-    components: {PageTitle},
-    data() {
-      return {
-        pageTitle: "REQUEST",
-        pageDetail: "たくさんの作品に参加させていただきました",
-      }
-    }
+    name: "request"
   }
 </script>
+
+<style scoped>
+
+</style>
